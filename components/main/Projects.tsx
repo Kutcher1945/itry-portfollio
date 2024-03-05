c06254a52fb6e106d594c80050973fb7c659614b
Adilan <adilan.akhramovich@gmail.com>
2024-03-05 20:20:33 +0500

    added a new images

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -12,19 +12,19 @@ const Projects = () => {
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/HtmlWebsite.png"
+          title="Modern HTML, CSS and JS Company Portfolio"
+          description="This is the undeniable beauty of the classic design of frame structures, increased requirements for energy efficiency, a variety of elements and types of opening, many color options, real Swedish quality multiplied by a large amount of testing."
         />
         <ProjectCard
           src="/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          title="Interactive Website with 3D house"
+          description="In ITRY Data is reality. If you face it, you can understand it."
         />
         <ProjectCard
           src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          title="iTry Fairy Forest Website using Paralax Technology"
+          description="Welcome to the iTry Parallax Forest website, where adventure meets technology in an immersive online experience!"
         />
       </div>
     </div>
